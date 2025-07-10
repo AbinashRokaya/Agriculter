340a73e4f5be07c36452248a7eeccdef4cfff8a7
Abirokaya <rokayaabinash9@gmail.com>
2025-07-11 03:15:44 +0545

    update

--- schemas/order_schema.py
+++ schemas/order_schema.py
@@ -3,6 +3,7 @@ from typing import Annotated,Optional,List
 from uuid import UUID,uuid4
 from enum import Enum
 from schemas.user_schema import UserResponse
+from schemas.product_schema import ProductPaganationResponse
 
 class OrderStatus(str,Enum):
     Pending="Pending"
@@ -28,7 +29,7 @@ class OrderResponse(BaseModel):
     total_amount:Optional[float]=None
     status:Optional[OrderStatus]=None
 
-    
+
 
 
 
@@ -45,4 +46,9 @@ class OrderIteamRequest(BaseModel):
         Field(...,ge=0, description="Price with 2 decimal places")
     ]
 
-
+class OrderIteamResponse(BaseModel):
+    order_item_id:Optional[UUID]=None
+    order_id=Optional[UUID]=None
+    product=Optional[ProductPaganationResponse]=None
+    stock_quanty=Optional[int]=None
+    price:Optional[float]=None
